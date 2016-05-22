1f07f80b89983b8a3e101f0a7f13be7c9a2af84a
liuchangyang112 <liuchangyang@yahoo.com.sg>
2016-05-22 14:27:59 +0800

    Add files via upload

---
+++ stepper_motor_control_updated_.ino
@@ -0,0 +1,77 @@
+// NOTE Maximum and minimum angularspeed may vary 
+
+int M1dirpin = 4;
+int M1steppin = 5;
+int M2dirpin = 7;
+int M2steppin = 6;
+int steps;
+float angularspeed; // 110-2600deg/s 
+
+void motor1(float M1speed, float degs){ //M1speed and M2speed from -100 to 100
+  steps=degs/1.8;
+  if(M1speed>0){
+    digitalWrite(M1dirpin,LOW);
+  }else if (M1speed<0){
+    digitalWrite(M1dirpin,HIGH);
+  }else{
+    steps=0;
+  }
+  angularspeed=100+abs(M1speed*25);
+  int t1=(int)(1000000/angularspeed)*1.8;
+  delayMicroseconds(2);
+  //int StartTime;
+  //StartTime=millis();
+  for(int j=0;j<steps;j++){
+    digitalWrite(M1steppin,LOW);
+    delayMicroseconds(2); //maxspeed=1 minspeed=900000
+    digitalWrite(M1steppin,HIGH);
+    delayMicroseconds(t1); //maxspeed=900 minspeed=90000
+  }
+ /*int CurrentTime = millis();
+   int ElapsedTime = CurrentTime - StartTime;
+   Serial.println(ElapsedTime); */
+}
+
+void motor2(float M2speed, float degs){
+  steps=degs/1.8;
+  if(M2speed>0){
+    digitalWrite(M2dirpin,LOW);
+  }else if (M2speed<0){
+    digitalWrite(M2dirpin,HIGH);
+  }else{
+    steps=0;
+  }
+  angularspeed=100+abs(M2speed*25);
+  int t2=(int)(1000000/angularspeed)*1.8;
+  delayMicroseconds(2);
+  //int StartTime;
+  //for(int i=0;i<100;i++){ //testloop
+    //angularspeed=angularspeed+50;
+    //StartTime=millis();
+    for(int j=0;j<steps;j++){
+      digitalWrite(M2steppin,LOW);
+      delayMicroseconds(2); //maxspeed=1 minspeed=900000
+      digitalWrite(M2steppin,HIGH);
+      delayMicroseconds(t2); //maxspeed=900 minspeed=90000
+  }
+ //}
+ /*int CurrentTime = millis();
+   int ElapsedTime = CurrentTime - StartTime;
+   Serial.println(ElapsedTime); */
+}
+
+
+void setup()
+{
+  pinMode(M1dirpin,OUTPUT);
+  pinMode(M1steppin,OUTPUT);
+  pinMode(M2dirpin,OUTPUT);
+  pinMode(M2steppin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+motor1(50, 360);
+motor2(50, 360);
+}
