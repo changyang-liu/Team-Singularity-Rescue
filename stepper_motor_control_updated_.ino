eb41c0f27c1d23b00b1cb54ef9bc36b1bb7c4c56
liuchangyang112 <liuchangyang@yahoo.com.sg>
2016-05-25 22:11:38 +0800

    Update stepper_motor_control_updated_.ino

--- stepper_motor_control_updated_.ino
+++ stepper_motor_control_updated_.ino
@@ -1,5 +1,5 @@
-// NOTE Maximum and minimum angularspeed may vary 
-// 1/8 step for driver -- change the steps to improve smoothness
+// NOTE 1/8 step for driver -- change mirostep to improve smoothness
+// angularspeed varies with microstep
 
 int M1dirpin = 4;
 int M1steppin = 5;
@@ -45,20 +45,17 @@ void motor2(float M2speed, float degs){
   angularspeed=0.5*(100+abs(M2speed*25));
   int t2=(int)(1000000/angularspeed)*0.225;
   delayMicroseconds(2);
-  int StartTime;
-  //for(int i=0;i<100;i++){ //testloop
-    //angularspeed=angularspeed+50;
-   StartTime=millis();
-    for(int j=0;j<steps;j++){
-      digitalWrite(M2steppin,LOW);
-      delayMicroseconds(2); //maxspeed=1 minspeed=900000
-      digitalWrite(M2steppin,HIGH);
-      delayMicroseconds(t2); //maxspeed=900 minspeed=90000
+  //int StartTime;
+  //StartTime=millis();
+  for(int j=0;j<steps;j++){
+    digitalWrite(M2steppin,LOW);
+    delayMicroseconds(2); //maxspeed=1 minspeed=900000
+    digitalWrite(M2steppin,HIGH);
+    delayMicroseconds(t2); //maxspeed=900 minspeed=90000
   }
- //}
- int CurrentTime = millis();
-   int ElapsedTime = CurrentTime - StartTime;
-   Serial.println(ElapsedTime); 
+/*int CurrentTime = millis();
+ int ElapsedTime = CurrentTime - StartTime;
+ Serial.println(ElapsedTime);*/ 
 }
 
 
@@ -73,10 +70,6 @@ void setup()
 
 void loop()
 {
-motor1(0, 360);
-motor2(100, 360);
-//delay(500);
-//motor2(-10, 50);
-//delay(500);
-//motor2(5, 60);
+motor1(50, 360);
+motor2(50, 360);
 }
