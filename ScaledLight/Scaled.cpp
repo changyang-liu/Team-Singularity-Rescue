9ff5a478fb8e18066f1d8bdcf2b1fa1015fcdc04
arianea <proandexpert@yahoo.com>
2016-06-06 17:53:06 +0800

    Update Scaled.cpp

--- ScaledLight/Scaled.cpp
+++ ScaledLight/Scaled.cpp
@@ -20,7 +20,7 @@ int Scaled::scale1()
 {
   raw1 = analogRead(sensorPin1); 
   scaled1 = ((raw1-417)/range1) * 100;  
-  if(scaled1<0) {scaled1=0}
+  if(scaled1<0) {scaled1=0;}
 
 
 return scaled1;
@@ -30,7 +30,7 @@ int Scaled::scale2()
 {
 raw2 = analogRead(sensorPin2);
   scaled2 = ((raw2-440)/range2) * 100;
-  if(scaled2<0) {scaled2=0}
+  if(scaled2<0) {scaled2=0;}
 return scaled2;
 }
 
@@ -38,7 +38,7 @@ int Scaled::scale3()
 {
   raw3 = analogRead(sensorPin3);
   scaled3 = ((raw3-386)/range3) * 100;
-    if(scaled3<0) {scaled3=0}
+    if(scaled3<0) {scaled3=0;}
 
 
 return scaled3;
@@ -48,7 +48,7 @@ int Scaled::scale4()
 { 
  raw4 = analogRead(sensorPin4);
   scaled4 = ((raw4-415)/range4) * 100;
-    if(scaled4<0) {scaled4=0}
+    if(scaled4<0) {scaled4=0;}
 
 return scaled4;
 }
