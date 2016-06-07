e002f236e7aa2857b2b72eaf0b8eca3659be7611
liuchangyang112 <liuchangyang@yahoo.com.sg>
2016-06-07 19:05:35 +0800

    Add files via upload
    log raw values

--- ScaledLight/Scaled.cpp
+++ ScaledLight/Scaled.cpp
@@ -5,7 +5,7 @@
 Scaled::Scaled() 
 {}
 
-
+/*
 int Scaled::scale1()
 {
   raw1 = analogRead(sensorPin1); 
@@ -46,16 +46,59 @@ if (scaled4 <0) {scaled4=0;}
 
 return scaled4;
 }
+*/
+
+int Scaled::scale1()
+{
+  raw1 = log(analogRead(sensorPin1)); 
+  scaled1 = ((raw1-4.75)/range1) * 100;  
+	if (scaled1 <0) {scaled1=0;}
+	if (scaled1 >100) {scaled1=100;}
+
+
+return scaled1;
+}
+
+int Scaled::scale2()
+{
+raw2 = log(analogRead(sensorPin2));
+  scaled2 = ((raw2-5.23)/range2) * 100;
+if (scaled2 <0) {scaled2=0;}
+	if (scaled2 >100) {scaled2=100;}
+
+return scaled2;
+}
+
+int Scaled::scale3()
+{
+  raw3 = log(analogRead(sensorPin3));
+  scaled3 = ((raw3-4.44)/range3) * 100;
+if (scaled3 <0) {scaled3=0;}
+	if (scaled3 >100) {scaled3=100;}
+
+return scaled3;
+}
+
+int Scaled::scale4()
+{ 
+ raw4 = log(analogRead(sensorPin4));
+  scaled4 = ((raw4-4.72)/range4) * 100;
+if (scaled4 <0) {scaled4=0;}
+	if (scaled4 >100) {scaled4=100;}
+
+return scaled4;
+}
+
 
 void Scaled::print()
-{ Serial.print("1: ");
-Serial.print(scale1());
-Serial.print(" 2: ");
-Serial.print(scale2());
-Serial.print(" 3: ");
-Serial.print(scale3());
-Serial.print(" 4: ");
-Serial.println(scale4());
+{ Serial.print("Far left: ");
+Serial.print(log(analogRead(sensorPin1)));
+Serial.print(" Close left: ");
+Serial.print(log(analogRead(sensorPin2)));
+Serial.print(" Close right: ");
+Serial.print(log(analogRead(sensorPin3)));
+Serial.print(" Far right: ");
+Serial.println(log(analogRead(sensorPin4)));
 }
 
 
--- ScaledLight/Scaled.h
+++ ScaledLight/Scaled.h
@@ -8,7 +8,7 @@ class Scaled
   public:
         Scaled ();	  
 	int scale1();
-	  int scale2();
+	int scale2();
 	int scale3 ();
 	int scale4 ();
 	void print ();
@@ -18,10 +18,17 @@ class Scaled
         int sensorPin3 = A12;
         int sensorPin4 = A11;
 
-        float range1 = 330 - 50;
-        float range2 = 816 - 120;
-        float range3 = 513 - 80;
-        float range4 = 870 - 160;
+        // float range1 = 330 - 50;
+        // float range2 = 816 - 120;
+        // float range3 = 513 - 80;
+        // float range4 = 870 - 160;
+
+	
+	float range1 = 6.45 - 4.75;
+	float range2 = 6.79 - 5.23;
+	float range3 = 6.26 - 4.44;
+	float range4 = 6.76 - 4.72;
+
 
         int scaled1;
         int scaled2;
