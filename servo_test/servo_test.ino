53ef34d7f1a44ca7e99d1220cc327542eab1fe8d
liuchangyang112 <liuchangyang@yahoo.com.sg>
2016-06-21 22:34:42 +0800

    update

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -2,12 +2,16 @@
 
 Servo myservo; 
 
+
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  servoPos(140);
+  delay(1000);
+  servoPos(0);
 }
 
 void loop() {
-  servoPos(0);
+  
 }
 
 void servoPos(int newPos){
@@ -15,15 +19,15 @@ void servoPos(int newPos){
   if(currentPos < newPos){
     for (int pos = currentPos; pos < newPos; pos++) { 
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(50);
+      delay(30);
     }   
   }else{
     for (int pos = currentPos; pos > newPos; pos--) { // goes from 180 degrees to 0 degrees
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      if(myservo.read() > 60){
-        delay(10);  
+      if(pos > 60){
+        delay(30);  
       }else{
-        delay(200);
+        delay(10);
       }
     }
   }
