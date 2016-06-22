9a3954978e868702077d2327cb1c19c0cfb215e0
Arianea <proandexpert@yahoo.com>
2016-06-22 22:13:09 +0800

    AA

--- pide_test/pide_test.ino
+++ pide_test/pide_test.ino
@@ -8,6 +8,7 @@
 #include <PIDe.h>
 #include <ColourSensor.h>
 #include <MotorFunctions.h>
+#include <EnableInterrupt.h>
 
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
 DualVNH5019MotorShield md;
@@ -22,6 +23,11 @@ Motors mtr = Motors(md);
 int LGreen;
 int RGreen;
 
+int loops = 0;
+int maxloops = 200;
+
+int Lr1, Lg1, Lb1, Lr2, Lg2, Lb2, Rr1, Rg1, Rb1, Rr2, Rg2, Rb2, LrAvg, LgAvg, LbAvg, RrAvg, RgAvg, RbAvg;
+
 void doEncoderA1(){mtr.getPastB1()?mtr.subtrEncoder1():mtr.addEncoder1();}
 void doEncoderA2(){mtr.getPastB2()?mtr.subtrEncoder2():mtr.addEncoder2();}
 void doEncoderB1(){mtr.setPastB1(!mtr.getPastB1());}
@@ -29,12 +35,11 @@ void doEncoderB2(){mtr.setPastB2(!mtr.getPastB2());}
 
 void setup() {
   ini.initialize();
-
-  attachInterrupt(digitalPinToInterrupt(mtr.getEncoder1PinA()), doEncoderA1, RISING);
-  attachInterrupt(digitalPinToInterrupt(mtr.getEncoder2PinA()), doEncoderA2, RISING);
-  attachInterrupt(digitalPinToInterrupt(mtr.getEncoder1PinB()), doEncoderB1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(mtr.getEncoder2PinB()), doEncoderB2, CHANGE);
-  //!accel.begin();
+  enableInterrupt(mtr.getEncoder1PinA(), doEncoderA1, RISING);
+  enableInterrupt(mtr.getEncoder2PinA(), doEncoderA2, RISING);
+  enableInterrupt(mtr.getEncoder1PinB(), doEncoderB1, CHANGE);
+  enableInterrupt(mtr.getEncoder2PinB(), doEncoderB2, CHANGE);
+  !accel.begin();
   md.init();
 
 }
@@ -54,6 +59,12 @@ void loop(){
 //  Serial.print(colour3.g());
 //  Serial.print("  ");
 //  Serial.println(colour3.b());
+//  if(70<colour2.r()<210 && 90<colour2.g()<235 && 75<colour2.b()<205 && abs(colour2.g()-colour2.r()) >20 && abs(colour2.g()-colour2.b()) >20) { 
+//    Serial.println("LGreen");
+//  }
+//  if (70<colour3.r()<220 && 85<colour3.g()<230 && 75<colour3.b()<210 && abs(colour3.g()-colour3.r())>20 && abs(colour3.g()-colour3.b())>20) {
+//  Serial.println("RGreen");
+//  }
 
   
   if(!ini.button()){
@@ -72,31 +83,27 @@ void loop(){
 //      md.setSpeeds(70, 40);
 //    }
 //  }
+
+    if(loops == maxloops){
+      if(slope() == 1){
+        pid.setMaxSpeed(150);
+      }else if (slope() == -1){
+        pid.setMaxSpeed(10);
+      }else{
+        pid.setMaxSpeed(70);
+      }
+      //Serial.println(gradient);
+      loops = 0;
+    }
+    ++loops;
   
-    if((far_left + close_left*1.5)/2.5 < 40 && (far_right + close_right*1.5)/2.5 < 40){
+    if((far_left + close_left)/2 < 40 && (far_right + close_right)/2 < 40){
       md.setBrakes(400, 400);
-      delay(500);
-      colour2.update();
-      if(70<colour2.r()<210 && 90<colour2.g()<235 && 75<colour2.b()<205 && abs(colour2.g()-colour2.r())>20 && abs(colour2.g()-colour2.b())>20) {
-        Serial.println("LGreen");
-        delay(1000);
-        mtr.moveCounts(-50, -50, 50);
-        delay(200);
-        singleTrack(1, 1000);      //side, closeleft, closeright
-        LGreen = 1;
-      }
-      colour3.update();
-      if (70<colour3.r()<220 && 85<colour3.g()<230 && 75<colour3.b()<210 && abs(colour3.g()-colour3.r())>20 && abs(colour3.g()-colour3.b())>20) {
-        Serial.println("RGreen");
-        delay(1000);
-        mtr.moveCounts(-50, -50, 50);
-        delay(200);
-        singleTrack(2, 1000);
-        RGreen = 1;
-      }
+      Lcolour();
+      Rcolour();
+
       if (LGreen == 0 && RGreen == 0) {
-        //mtr.moveCounts(60, 60, 40);
-        delay(2000);
+        mtr.moveCounts(60, 60, 23);
         //md.setBrakes(400,400);
       }
       LGreen = 0;
@@ -126,4 +133,155 @@ void singleTrack(int side, long t){
 } 
 
 
+float accelX(){                                 //Accelerometer readings on X, Y and Z axis.
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  return event.acceleration.x;
+}
+
+float accelY(){
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  return event.acceleration.y;
+}
+
+float accelZ(){
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  return event.acceleration.z;
+}
+
+int slope(){                           //Function to detect uphill, downhill or flat
+if (((atan2(accelZ(),accelY()) * 180) / 3.1415926)>-100&&((atan2(accelZ(),accelY()) * 180) / 3.1415926)<-75)
+  {
+  return 0;}
+  else
+  {
+    if (((atan2(accelZ(),accelY()) * 180) / 3.1415926)>-90)
+    {return 1;}
+    else
+    {return -1;}
+  }
+}
+
+void Lcolour()    {colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+colour2.update();
+      Lr1 = colour2.r();
+      Lg1 = colour2.g();
+      Lb1 = colour2.b();
+      delay(100);
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+            colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+            colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      colour2.update();
+      Lr2 = colour2.r();
+      Lg2 = colour2.g();
+      Lb2 = colour2.b();
+      LrAvg = (Lr1+Lr2)/2;
+      LgAvg = (Lg1+Lg2)/2;
+      LbAvg = (Lb1+Lb2)/2;
+      if(70<LrAvg<210 && 90<LgAvg<235 && 75<LbAvg<205 && abs(LgAvg-LrAvg) >20 && abs(LgAvg-LbAvg) >20) {
+        Serial.println("L");
+        mtr.moveCounts(-50, -50, 50);
+        delay(100);
+        singleTrack(1, 1000);      //side, closeleft, closeright
+        LGreen = 1;
+      }
+}
+
+
+void Rcolour() { 
+        colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+                colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+                colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+        colour3.update();
+      Rr1 = colour3.r();
+      Rg1 = colour3.g();
+      Rb1 = colour3.b();
+      delay(100);
+      colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+            colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+            colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+      colour3.update();
+      Rr2 = colour3.r();
+      Rg2 = colour3.g();
+      Rb2 = colour3.b();
+      RrAvg = (Rr1+Rr2)/2;
+      RgAvg = (Rg1+Rg2)/2;
+      RbAvg = (Rb1+Rb2)/2;
+      delay(100);
+      if (70<RrAvg<220 && 85<RgAvg<230 && 75<RbAvg<210 && abs(RgAvg-RrAvg)>20 && abs(RgAvg-RrAvg)>20) {
+        Serial.println("R");
+        mtr.moveCounts(-50, -50, 50);
+        delay(100);
+        singleTrack(2, 1000);
+        RGreen = 1;
+      }
+
+}
 
