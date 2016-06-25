b1b0cd4e3c05836413bfd4ac5c9146e01e380365
Arianea <proandexpert@yahoo.com>
2016-06-25 11:05:15 +0800

    Updated ScaledLight library

--- libraries/ScaledLight/Scaled.cpp
+++ libraries/ScaledLight/Scaled.cpp
@@ -6,7 +6,7 @@ Scaled::Scaled()
 {}
 
 /*
-int Scaled::scale1()
+float Scaled::scale1()
 {
   raw1 = analogRead(sensorPin1); 
   scaled1 = ((raw1-50)/range1) * 100;  
@@ -17,7 +17,7 @@ int Scaled::scale1()
 return scaled1;
 }
 
-int Scaled::scale2()
+float Scaled::scale2()
 {
 raw2 = analogRead(sensorPin2);
   scaled2 = ((raw2-120)/range2) * 100;
@@ -27,7 +27,7 @@ if (scaled2 <0) {scaled2=0;}
 return scaled2;
 }
 
-int Scaled::scale3()
+float Scaled::scale3()
 {
   raw3 = analogRead(sensorPin3);
   scaled3 = ((raw3-80)/range3) * 100;
@@ -37,7 +37,7 @@ if (scaled3 <0) {scaled3=0;}
 return scaled3;
 }
 
-int Scaled::scale4()
+float Scaled::scale4()
 { 
  raw4 = analogRead(sensorPin4);
   scaled4 = ((raw4-160)/range4) * 100;
@@ -48,43 +48,43 @@ return scaled4;
 }
 */
 
-int Scaled::scale1()
+float Scaled::scale1()
 {
   raw1 = log(analogRead(sensorPin1)); 
   scaled1 = ((raw1-min1)/range1) * 100;  
 	if (scaled1 <0) {scaled1=0;}
-	if (scaled1 >100) {scaled1=100;}
+//	if (scaled1 >100) {scaled1=100;}
 
 
 return scaled1;
 }
 
-int Scaled::scale2()
+float Scaled::scale2()
 {
 raw2 = log(analogRead(sensorPin2));
   scaled2 = ((raw2-min2)/range2) * 100;
 if (scaled2 <0) {scaled2=0;}
-	if (scaled2 >100) {scaled2=100;}
+//	if (scaled2 >100) {scaled2=100;}
 
 return scaled2;
 }
 
-int Scaled::scale3()
+float Scaled::scale3()
 {
   raw3 = log(analogRead(sensorPin3));
   scaled3 = ((raw3-min3)/range3) * 100;
 if (scaled3 <0) {scaled3=0;}
-	if (scaled3 >100) {scaled3=100;}
+//	if (scaled3 >100) {scaled3=100;}
 
 return scaled3;
 }
 
-int Scaled::scale4()
+float Scaled::scale4()
 { 
  raw4 = log(analogRead(sensorPin4));
   scaled4 = ((raw4-min4)/range4) * 100;
 if (scaled4 <0) {scaled4=0;}
-	if (scaled4 >100) {scaled4=100;}
+//	if (scaled4 >100) {scaled4=100;}
 
 return scaled4;
 }
